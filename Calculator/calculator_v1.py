faefafe5c782179fd96507a3b9733b4baec2a1c4
Sabrina Castro <81202040+sabrinaamorimdecastro@users.noreply.github.com>
2021-05-04 19:06:39 -0300

    Add files via upload

--- Calculator/calculator_v1.py
+++ Calculator/calculator_v1.py
@@ -1,1 +1,35 @@
+# Calculadora em Python
+# By sabrinaamorimdecastro
+
+# Math library
+import math
+
+print("\n******************* Python Calculator *******************")
+
+# Choose operation 
+print("Select the number of the operation: \ n1 - Sum \ n2 - Subtraction \ n3 - Multiplication \ n4 - Division ")
+
+operacao = int(input("Input the operation (1|2|3|4): "))
+
+num1 = int(input("Input the first number: "))
+
+num2 = int(input("Input the second number: "))
+
+
+if operacao == 1:
+	sum = num1 + num2
+	print("%d + %d = %d" %(num1, num2, sum))
+elif operacao == 2:
+	sub = num1 - num2
+	print("%d - %d = %d" %(num1, num2, sub))
+elif operacao == 3:
+	mult = num1 * num2
+	print("%d * %d = %d" %(num1, num2, mult))
+elif operacao == 4:
+	div = num1 / num2
+	print("%d / %d = %d" %(num1, num2, div))
+else:
+	print("Input value for operation is incorrect. Please try again from 1 to 4.")
+
+
 
---
+++ Calculator/calculator_v2.py
@@ -0,0 +1,54 @@
+# Calculador in Python
+# Code created by cassab96
+
+# Development of the four basics operations using Python language 
+# Application of fundamental knowlegde - first challenge
+
+#import package
+import time
+import math
+
+print("\n----------> Python Calculator <----------\n")
+
+# Description
+print("Select the operation: \n\n           Sum = + \n   Subtraction = - \nMultiplication = * ou x \n      Division = / \n\n")
+
+def main():
+
+	# Choose a math operation
+	num1 = int(input("Number 1:      "))
+
+	# Input first number
+	operation = str(input("Operation:     "))
+
+	# Input second number
+	num2 = int(input("Number 2:      "))
+
+	# Program:
+	if operation == "+":
+		su = num1 + num2
+		print("             ______ \n\nResultado=     %d" %su)
+
+	elif operation == "-":
+		sub = num1 - num2
+		print("             ______ \n\nResultado=     %d" %sub)
+
+	elif operation == "*":
+		mult = num1 * num2
+		print("             ______ \n\nResultado=     %d" %mult)
+
+	elif operation == "x":
+		mult2 = num1 * num2
+		print("             ______ \n\nResultado=     %d" %mult2)
+
+	elif operation == "/":
+		div = num1 / num2
+		print("             ______ \n\nResultado=     %d" %div)
+		
+	else:
+		print("Input value for operation is incorrect. Please try again.")
+	
+	
+	time.sleep(3)
+if __name__ == "__main__":
+	main()
